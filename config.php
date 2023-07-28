a38f2f06b5d8bb1b64344406be1139191f516cb1
Davi Gledson <davigledson@outlook.com>
2023-07-28 01:50:32 -0300

    terminando aula 1

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
  
  const DBDRIVE = 'mysql';
  const DBHOST = 'localhost'; 
- const DBNAME = 'serie_login';
+ const DBNAME = 'api_rest';
  const DBBUSER = 'root';
  const DBPASS = '';
--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,36 @@
 <?php 
-
+header("Content-Type: application/json");
 require_once '../vendor/autoload.php';
+
+//api/users/1
+
+//explodir — Dividir uma string por uma string
+if($_GET['url']){
+    $url = explode('/', $_GET['url']);
+//var_dump($url);
+
+    if($url[0] === 'api'){
+        //Anexar elementos ao início de um array Desde 7.3.0 esta função pode ser chamada com apenas um parâmetro.
+        array_shift($url);
+
+        $service ='App\Services\\'. ucfirst($url[0]) . 'Service';
+        array_shift($url);
+        //r($url);
+
+        $method =strtolower($_SERVER['REQUEST_METHOD']);
+        //r($method);
+
+        try {
+          $response = call_user_func_array(array(new $service, $method),$url);
+            
+            http_response_code(200);
+
+          echo json_encode(array('status'=> 'sucess','data'=> $response));
+            exit;
+        } catch (\Exception $e) {
+            http_response_code(404);
+            echo json_encode(array('status'=> 'error','data'=> $e->getMessage()));
+            exit;
+        }
+    }
+}
